b9c0a39578ba887152a424de3ef39bfe940c3cff
Oleh Zankiv <oleh.zankiv@zealicsolutions.com>
2023-04-24 15:59:39 +0300

    Added environment file [module_3]

--- module_3/server.js
+++ module_3/server.js
@@ -1,5 +1,10 @@
 import { app } from "./app.js";
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000");
-});
+const PORT = process.env.PORT || 3000;
+
+const runServer = () =>
+  app.listen(PORT, () => {
+    console.log("Server running. Use our API on port: 3000");
+  });
+
+runServer();
