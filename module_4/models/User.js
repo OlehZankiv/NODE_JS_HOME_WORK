bb2dfe17a1a00f03aef6781e282e3a2718e99137
Oleh Zankiv <oleh.zankiv@zealicsolutions.com>
2023-04-26 14:44:44 +0300

    added eslint and prettier

--- module_4/models/User.js
+++ module_4/models/User.js
@@ -1,30 +1,34 @@
 import mongoose, { Schema } from "mongoose";
 import bcryptjs from "bcryptjs";
 
-const user = new Schema({
-  password: {
-    type: String,
-    required: [true, "Set password for user"],
+const user = new Schema(
+  {
+    password: {
+      type: String,
+      required: [true, "Set password for user"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: [true, "User should be uniq"],
+    },
+    subscription: {
+      type: String,
+      enum: ["starter", "pro", "business"],
+      default: "starter",
+    },
+    token: String,
   },
-  email: {
-    type: String,
-    required: [true, "Email is required"],
-    unique: [true, "User should be uniq"],
-  },
-  subscription: {
-    type: String,
-    enum: ["starter", "pro", "business"],
-    default: "starter",
-  },
-  token: String,
-});
-
-user.methods.setPassword = function (password) {
-  this.password = bcryptjs.hashSync(password, 8);
-};
-
-user.methods.checkPassword = function (password) {
-  return bcryptjs.compareSync(password, this.password);
-};
+  {
+    methods: {
+      setPassword: function (password) {
+        this.password = bcryptjs.hashSync(password, 8);
+      },
+      checkPassword: function (password) {
+        return bcryptjs.compareSync(password, this.password);
+      },
+    },
+  }
+);
 
 export const User = mongoose.model("users", user);
